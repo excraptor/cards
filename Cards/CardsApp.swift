9b912936ccd9883c0abfae63034f2e268b863774
Tamás Balla <tamas.balla@4ig.hu>
2022-06-17 23:13:04 +0200

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Balla Tamás on 2022. 06. 17..
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
